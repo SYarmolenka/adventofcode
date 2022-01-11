9fad9a20aebef4f2a5948734b79505c9753f33df
Sergey Ermolenko <ermolenko@medignition.com>
2022-01-11 12:56:40 +0300

    init hoce app, day1

---
+++ src/day1/index.js
@@ -0,0 +1,27 @@
+export const task1 = (arr) =>
+  arr.reduce(
+    (acc, n, index) => {
+      if (index === 0) return acc;
+      if (arr[index - 1] > n) acc.dec += 1;
+      if (arr[index - 1] < n) acc.inc += 1;
+
+      return acc;
+    },
+    { inc: 0, dec: 0 }
+  );
+
+export const task2 = (arr) =>
+  arr.reduce(
+    (acc, n, index) => {
+      if (index === 0 || index === 1 || index === acc.length - 1) return acc;
+
+      const sum1 = arr[index - 2] + arr[index - 1] + n;
+      const sum2 = arr[index - 1] + n + arr[index + 1];
+
+      if (sum1 > sum2) acc.dec += 1;
+      if (sum1 < sum2) acc.inc += 1;
+
+      return acc;
+    },
+    { inc: 0, dec: 0 }
+  );
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { data1 } from './data/sonar.js';
+import { task1, task2 } from './day1/index.js';
+
+console.log(task1(data1));
+console.log(task2(data1));
